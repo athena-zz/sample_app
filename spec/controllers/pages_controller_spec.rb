5b74025f39096c49be6f76988d765cba563f7fa9
Alexa Hirschfeld <alexa@paperlesspost.com>
2011-05-11 22:09:10 -0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,41 +4,54 @@ describe PagesController do
   render_views
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title",
+                                      :content => @base_title + " | Home")
+      end
     end
-    
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+
+    describe "GET 'contact'" do
+      it "should be successful" do
+        get 'contact'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'contact'
+        response.should have_selector("title",
+                                      :content => @base_title + " | Contact")
+      end
     end
-  end
+
+    describe "GET 'about'" do
+      it "should be successful" do
+        get 'about'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'about'
+        response.should have_selector("title",
+                                      :content => @base_title + " | About")
+      end
+    end
   
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
+    describe "GET 'help'" do
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'help'
+        response.should have_selector("title",
+                                      :content => @base_title + " | Help")
+      end
     end
-    
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
-    end
-  end
 end
